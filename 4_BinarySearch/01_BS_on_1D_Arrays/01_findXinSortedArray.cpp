7d81b83cb00e4132ba071b76f8b3a7d506f1af33
Gobinda03 <gobindahazra3103@gmail.com>
2025-08-11 17:43:22 +0530

    Binary search modified method

--- 4_BinarySearch/01_BS_on_1D_Arrays/01_findXinSortedArray.cpp
+++ 4_BinarySearch/01_BS_on_1D_Arrays/01_findXinSortedArray.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int f(int a[], int n, int t)
-{
+
+// Iterative Method
+int findK1(vector<int> &arr, int n, int t){
     int low = 0, high = n - 1;
-    while (low <= high)
-    {
+    while (low <= high){
         int mid = (low + high) / 2;
-        if (a[mid] == t)
-        {
-            return mid + 1;
+        if (arr[mid] == t){
+            return mid;
         }
-        else if (t > a[mid])
-        {
+        else if (t > arr[mid]){
             low = mid + 1;
         }
         else
@@ -19,20 +17,41 @@ int f(int a[], int n, int t)
     }
     return -1;
 }
+
+
+
+// recursive method
+int search(vector<int>&arr, int low, int high, int k){
+    if(low>high) return -1;
+
+    int mid = (high + low) /2;
+    if(arr[mid] == k) return mid;
+    else if(arr[mid] > k) return search(arr, low, mid-1, k);
+    else return search(arr, mid+1, high, k);
+}
+int foundTarget2(vector<int>&arr, int n, int k){
+    if(arr.empty()) return 0;
+
+    return search(arr, 0, n-1, k);
+}
+
+
+// TC = O(log2  N)
+
+
 int main()
 {
-    int n, terget;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
+    vector<int>arr = {3, 4, 6, 7, 9, 12, 16, 17};
+    int target = 6;
+    int n = arr.size();
+    for(auto it: arr){
+        cout<<it<<" ";
     }
-    int ans;
-    cout << "Target";
-    cin >> terget;
-    ans = f(arr, n, terget);
-    cout << ans;
+    cout<<endl;
+
+    cout<<target<<" found at index "<<findK1(arr, n, target)<<" using iterative method"<<endl;
+    cout<<target<<" found at index "<<foundTarget2(arr, n, target)<<" using recursive method"<<endl;
+
 
     return 0;
 }
