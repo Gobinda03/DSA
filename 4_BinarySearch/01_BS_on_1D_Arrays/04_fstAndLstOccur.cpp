3d69858fcecd01fc5f04528faa3abac49f1c71ff
Gobinda03 <gobindahazra3103@gmail.com>
2025-08-13 09:17:23 +0530

    First and Last occurrences of element in array

---
+++ 4_BinarySearch/01_BS_on_1D_Arrays/04_fstAndLstOccur.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// First occur
+int firstOccur(vector<int> &arr, int n, int target){
+    int low = 0, high = n-1;
+    int ans = -1;
+
+    while(low<=high){
+        int mid = (low+high)/2;
+        
+        if(arr[mid] == target){
+            ans = mid;
+            high = mid -1;
+        }
+        else if(target>arr[mid]){
+            low = mid + 1;
+        }
+        else{
+            high = mid -1;
+        }
+    }
+    return ans;
+}
+
+// last Occur
+int lastOccur(vector<int>&arr, int n, int target){
+        int low = 0, high = n-1;
+        int ans = -1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        
+        if(arr[mid] == target){
+            ans = mid;
+            low = mid + 1;
+        }
+        else if(target>arr[mid]){
+            low = mid + 1;
+        }
+        else{
+            high = mid -1;
+        }
+    }
+    return ans;
+}
+
+
+// TC = O(2log2  N)
+
+
+int main()
+{
+    vector<int>arr = {3, 4, 6, 7, 7, 7, 9, 12,  16, 17};
+    int target = 7;
+    int n = arr.size();
+    for(auto it: arr){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    // auto lb = lower_bound(arr.begin(), arr.end(), target) - arr.begin();
+    // cout<<lb;
+
+
+    cout<<target<<" occur for first time at index "<<firstOccur(arr, n, target)<<endl;
+    cout<<target<<" occur for last time at index "<<lastOccur(arr, n, target)<<endl;
+
+
+    return 0;
+}
