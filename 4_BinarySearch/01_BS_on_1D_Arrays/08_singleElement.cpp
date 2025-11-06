574573c9e7a764627237be21a24360c6960c13ec
Gobinda03 <gobindahazra3103@gmail.com>
2025-11-06 20:20:50 +0530

    Finding the Singlr Element in a sorted array

---
+++ 4_BinarySearch/01_BS_on_1D_Arrays/08_singleElement.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleElement(vector<int> &arr, int n)
+{
+    if (n == 1)
+        return arr[0];
+    if (arr[0] != arr[1])
+        return arr[0];
+    if (arr[n - 1] != arr[n - 2])
+        return arr[n - 1];
+
+    int low = 2, high = n - 2, mid;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
+            return arr[mid];
+
+        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1]) || (mid % 2 == 0 && arr[mid] == arr[mid + 1]))
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> arr = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10};
+    int n = arr.size();
+    for (auto it : arr)
+    {
+        cout << it << " ";
+    }
+
+    int ans = singleElement(arr, n);
+    cout <<"\nThe single Element is "<< ans;
+    return 0;
+}
