bb4274eafedfc1205bacf57ff0a1121dfe20b2cf
Gobinda03 <gobindahazra3103@gmail.com>
2026-01-01 21:33:09 +0530

    Isomorphic String

---
+++ 5_Strings/05_isomorphicString.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isomorphicString(string s, string t) {
+    int m1[256] = {0}, m2[256] = {0}; 
+    
+    int n = s.size(); 
+    
+    for (int i = 0; i < n; ++i) {
+        if (m1[s[i]] != m2[t[i]]) return false;
+        
+        m1[s[i]] = i + 1;
+        m2[t[i]] = i + 1;
+    }
+    
+    return true;
+}
+
+  
+int main() {
+    string s = "paper";
+    string t = "tinle";
+    
+    if (isomorphicString(s, t)) {
+        cout << "Strings are isomorphic." << endl;
+    } else {
+        cout << "Strings are not isomorphic." << endl;
+    }
+    
+    return 0;
+}
