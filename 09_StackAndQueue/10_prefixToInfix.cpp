6b04e766ba4c5960bdf7e08b14a7febd2750d3cf
Gobinda03 <gobindahazra3103@gmail.com>
2026-02-03 10:51:49 +0530

    Prefix To Postfix

---
+++ 09_StackAndQueue/10_prefixToInfix.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Prefix To Infix
+// Tc = O(N), SC = O(N)
+string prefixToInfix(string s){
+    int i = s.length() - 1;
+    stack<string>st;
+
+    while(i >= 0){
+        if((s[i] >= 'a' && s[i] <= 'z')||
+        (s[i] >= 'A' && s[i] <= 'Z')||
+        (s[i] >= '0' && s[i] <= '9')){
+            st.push(string(1, s[i]));
+        }else{
+            if(st.size() < 2) return "";
+            string op1 = st.top(); 
+            st.pop();
+            string op2 = st.top();
+            st.pop();
+
+            string eq = "(" + op1 + s[i] + op2 + ")";
+            st.push(eq);
+        }
+        i--;
+    }
+
+    return st.top();
+}
+
+int main()
+{
+    string s = "*+pq-mn";
+    cout<<s<<endl;
+    cout<<prefixToInfix(s);
+    return 0;
+}
