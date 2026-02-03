e77da1e38aca10bf41220ca33dca75ef751a1557
Gobinda03 <gobindahazra3103@gmail.com>
2026-02-03 10:37:24 +0530

    Postfix To Infix

---
+++ 09_StackAndQueue/09_postfixToInfix.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Postfix To Infix
+// Tc = O(N), SC = O(N)
+string postfixToInfix(string s){
+    int i = 0;
+    stack<string>st;
+
+    while(i < s.length()){
+        if((s[i] >= 'a' && s[i] <= 'z')||
+        (s[i] >= 'A' && s[i] <= 'Z')||
+        (s[i] >= '0' && s[i] <= '9')){
+            st.push(string(1, s[i]));
+        }else{
+            if(st.size() < 2) return "";
+            string op1 = st.top(); 
+            st.pop();
+            string op2 = st.top();
+            st.pop();
+
+            string eq = "(" + op2 + s[i] + op1 + ")";
+            st.push(eq);
+        }
+        i++;
+    }
+
+    return st.top();
+}
+
+int main()
+{
+    string s = "AB-DE+F*/";
+    cout<<s<<endl;
+    cout<<postfixToInfix(s);
+    return 0;
+}
